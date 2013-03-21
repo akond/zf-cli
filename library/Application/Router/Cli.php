cf3e0fdaa3ef6cadd2acf20c370d1f6d76c02393
Adrian World <aw@adrianworlddesign.com>
2013-03-21 09:48:40 -0500

    Update Cli.php
    Support for params in request object.
    
    Example: php cli.php index test param=one

--- library/Application/Router/Cli.php
+++ library/Application/Router/Cli.php
@@ -27,6 +27,15 @@ class Application_Router_Cli extends Zend_Controller_Router_Abstract
 				{
 					echo "Invalid action $action.\n", exit ();
 				}
+
+		                if ( !empty($arguments) ) {
+		                    foreach ($arguments as $arg ) {
+		                        $arg_pair = explode('=', $arg);
+		                        if ( 2 === count($arg_pair) ) {
+		                            $dispatcher->setParam($arg_pair[0], $arg_pair[1]);
+		                        }
+		                    }
+		                }				
 			}
 		}
 
